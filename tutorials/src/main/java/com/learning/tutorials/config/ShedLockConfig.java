cccceb668fa4b143e5c0a9995afcf273b59cabe4
TharinduShantha98 <tharindushantha4@gmail.com>
2025-06-19 23:15:40 +0530

    shedlock

---
+++ tutorials/src/main/java/com/learning/tutorials/config/ShedLockConfig.java
@@ -0,0 +1,11 @@
+package com.learning.tutorials.config;
+
+
+
+@org.springframework.context.annotation.Configuration
+
+
+
+
+public class Configuration {
+}
---
+++ tutorials/src/main/java/com/learning/tutorials/service/ScheduleService.java
@@ -0,0 +1,4 @@
+package com.learning.tutorials.service;
+
+public class ScheduleService {
+}
