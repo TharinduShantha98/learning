50cf0d2b7f2ddb573c3981d605b5f69d7edb11ed
TharinduShantha98 <tharindushantha4@gmail.com>
2025-06-19 23:16:13 +0530

    added shedlock

--- tutorials/src/main/java/com/learning/tutorials/TutorialsApplication.java
+++ tutorials/src/main/java/com/learning/tutorials/TutorialsApplication.java
@@ -2,12 +2,17 @@ package com.learning.tutorials;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TutorialsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialsApplication.class, args);
 	}
+
+
+
 
 }
--- tutorials/src/main/java/com/learning/tutorials/service/ScheduleService.java
+++ tutorials/src/main/java/com/learning/tutorials/service/ScheduleService.java
@@ -1,4 +1,20 @@
 package com.learning.tutorials.service;
 
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class ScheduleService {
+
+
+    @Scheduled(fixedRate = 2000)
+    @SchedulerLock(name = "sendEmailPromo",lockAtMostFor = "1m",lockAtLeastFor = "5s")
+    public void sendEmailPromotion() {
+        System.out.println("Scheduled task at " + new Date());
+
+    }
+
 }
